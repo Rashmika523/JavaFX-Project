9bc68e8ee98826af41c0ec75d41cc5b7ad10812d
Gunathilaka <anjurashmika188@gmail.com>
2024-06-29 10:58:02 +0530

    Created SignupForm UI and Generate components ids in controller class

---
+++ src/com/insitute/iimanage/controller/SignupFormController.java
@@ -0,0 +1,21 @@
+package com.insitute.iimanage.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignupFormController {
+
+    public AnchorPane context;
+    public TextField txtFirstName;
+    public PasswordField txtPassword;
+    public TextField txtLastName;
+    public TextField txtEmail;
+
+    public void singupOnAction(ActionEvent actionEvent) {
+    }
+
+    public void alreadyHaveanAccountOnAction(ActionEvent actionEvent) {
+    }
+}
