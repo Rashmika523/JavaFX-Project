2561eb05b18c3f03e6867d2551f4714206a91857
Gunathilaka <anjurashmika188@gmail.com>
2024-06-29 12:34:46 +0530

    created dashboard form UI and generated components in dashboard controller class

---
+++ src/com/insitute/iimanage/controller/DashboardController.java
@@ -0,0 +1,29 @@
+package com.insitute.iimanage.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class DashboardController {
+    public AnchorPane context;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void studentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void teacherOnAction(ActionEvent actionEvent) {
+    }
+
+    public void intakeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void courseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void RegistrationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void logoutOnAction(ActionEvent actionEvent) {
+    }
+}
