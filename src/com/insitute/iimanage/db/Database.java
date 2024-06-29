8aa19256ed54a7dca5653b93df7a891bf9060ffd
Gunathilaka <anjurashmika188@gmail.com>
2024-06-29 11:25:04 +0530

    created User bean and User data table

--- src/com/insitute/iimanage/db/Database.java
+++ src/com/insitute/iimanage/db/Database.java
@@ -1,4 +1,11 @@
 package com.insitute.iimanage.db;
 
+import com.insitute.iimanage.model.User;
+
+import java.util.ArrayList;
+
 public class Database {
+
+    public static ArrayList<User> userTable = new ArrayList();
+
 }
