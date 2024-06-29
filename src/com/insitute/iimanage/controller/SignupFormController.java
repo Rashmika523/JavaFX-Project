d469cb7055a49493b9475d949147339b9b3fbcbc
Gunathilaka <anjurashmika188@gmail.com>
2024-06-29 11:33:01 +0530

    created new user and saved it into usertable

--- src/com/insitute/iimanage/controller/SignupFormController.java
+++ src/com/insitute/iimanage/controller/SignupFormController.java
@@ -1,14 +1,19 @@
 package com.insitute.iimanage.controller;
 
+import com.insitute.iimanage.db.Database;
+import com.insitute.iimanage.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.DataInput;
 import java.io.IOException;
+import java.util.Locale;
 
 public class SignupFormController {
 
@@ -18,7 +23,19 @@ public class SignupFormController {
     public TextField txtLastName;
     public TextField txtEmail;
 
-    public void singupOnAction(ActionEvent actionEvent) {
+    public void singupOnAction(ActionEvent actionEvent) throws IOException {
+
+        String firstName = txtFirstName.getText().trim().toLowerCase();
+        String lastname = txtLastName.getText().trim().toLowerCase();
+        String email = txtEmail.getText().trim().toLowerCase();
+        String password = txtPassword.getText().trim();
+
+        Database.userTable.add(
+                new User(firstName,lastname,email,password)
+        );
+        new Alert(Alert.AlertType.CONFIRMATION,"Your Account has been Created...!").show();
+        setUI("LoginForm");
+
     }
 
     public void alreadyHaveanAccountOnAction(ActionEvent actionEvent) throws IOException {
