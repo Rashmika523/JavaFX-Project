ea617a15539558fb602933b68c9c93570ad31c7f
Gunathilaka <anjurashmika188@gmail.com>
2024-06-29 10:45:25 +0530

    Created LoginForm UI and Generate components ids in controller class

--- src/com/insitute/iimanage/controller/LoginFormController.java
+++ src/com/insitute/iimanage/controller/LoginFormController.java
@@ -1,4 +1,20 @@
 package com.insitute.iimanage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+    public Hyperlink txtForgotPassword;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
