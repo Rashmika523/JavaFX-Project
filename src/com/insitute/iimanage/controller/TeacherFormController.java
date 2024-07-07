0abf5b25640c94bffe093ad8d227f5176e079018
Gunathilaka <anjurashmika188@gmail.com>
2024-07-07 09:39:03 +0530

    Generated TeacherID

--- src/com/insitute/iimanage/controller/TeacherFormController.java
+++ src/com/insitute/iimanage/controller/TeacherFormController.java
@@ -1,11 +1,19 @@
 package com.insitute.iimanage.controller;
 
+import com.insitute.iimanage.db.Database;
+import com.insitute.iimanage.model.Student;
+import com.insitute.iimanage.model.Teacher;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TeacherFormController {
     public AnchorPane context;
@@ -22,12 +30,43 @@ public class TeacherFormController {
     public TableColumn colOption;
     public TextField txtContact;
 
+    public void initialize(){
+        generateTeacherID();
+    }
+
     public void newTeacherOnAction(ActionEvent actionEvent) {
     }
 
-    public void backToHomeOnAction(ActionEvent actionEvent) {
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("Dashboard");
     }
 
     public void saveTeacherOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
+    }
+
+    private void generateTeacherID() {
+
+        if (!Database.teacherTable.isEmpty()) {
+
+            Teacher lastTeacher = Database.teacherTable.get(Database.teacherTable.size() - 1);
+            String stringId = lastTeacher.getTeacherId();
+            String[] split = stringId.split("-");
+            String lastIdAsString = split[1];
+            int lastIdAsInteger = Integer.parseInt(lastIdAsString);
+            lastIdAsInteger++;
+            String newId = "T-" + lastIdAsInteger;
+            textTeacherID.setText(newId);
+
+        } else {
+            textTeacherID.setText("T-1");
+        }
+
     }
 }
